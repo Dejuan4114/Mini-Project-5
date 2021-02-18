4f068aab427ff77d23802c11cc783a34fcaeb288
Dejuan4114 <dejuan.whipple@patelhs.org>
2021-02-18 17:39:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+ System.out.println("Hey! I have some new awesome pants for sale. How much money do you guys have?");
+ {
+   scanner scan = new Scanner (System.in);
+ } 
+  }
+}
